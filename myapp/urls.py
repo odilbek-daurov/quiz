fe4b89592c612d2e437946e026b88766ce853dd3
Odilbek <daurovodilbek@gmail.com>
2022-03-27 14:42:20 +0500

    registretsiya

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import questions, topic, login
+from .views import questions, topic, login_view, result, logout_view, signup
 
 urlpatterns = [
-    path('', topic, name = 'topic' ),
-    path('', login, name = 'login' ),
-    path('test/<slug:slug>', questions, name = 'question' ),
+    path('', topic, name='topic'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', signup, name='signup'),
+    path('test/<slug:slug>', questions, name = 'question'),
+    path('results/', result, name='result')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,47 +1,87 @@
-from ast import Pass
+
 from django.shortcuts import render, redirect
 from .models import Topic, Answer, Questions, Result
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from random import  shuffle
+from random import shuffle
+from .forms import RegisterForm
 
-def login(request):
+
+def signup(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data = request.POST)
+        form = RegisterForm(data=request.POST)
         if form.is_valid():
-            user = form.get()
+            user = form.save()
             login(request, user)
-            return redirect('blog_list')
-    
-    return render(request, 'login.html')
+            return redirect('topic')
+    form = RegisterForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('topic')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
 
 def topic(request):
-    topic = Topic.objects.all()
+    topics = Topic.objects.all()
     context = {
-        'topic':topic
+        'topics': topics,
     }
-    
+
     return render(request, 'home.html', context)
 
 
 def questions(request, slug):
-    topic = Topic.objects.get(slug = slug)
-    question = Questions.objects.filter(topic = topic)
+    topic = Topic.objects.get(slug=slug)
+    question = Questions.objects.filter(topic=topic)
     ansver = list(Answer.objects.all())
-     
-    if request.method == 'POST':
-        Result.objects.create(
-            user = request.user
-            
-        )
-         
-    
     shuffle(ansver)
+    if request.method == 'POST':
+        correct = 0
+        wrong = 0
+        for q in question:
+            if request.POST.get(q.name) == 'True':
+                correct += 1
+            else:
+                wrong += 1
+
+        Result.objects.create(
+            user=request.user,
+            correct=correct,
+            topic=topic,
+            total_question=len(question),
+
+        )
+
+        context = {
+            'user': request.user.username,
+            'correct': correct,
+
+        }
+        return render(request, 'result_list.html', context)
+
     context = {
-        'question':question,
-        'ansver':ansver,
+        'question': question,
+        'ansver': ansver,
     }
-    
+
     return render(request, 'quiz.html', context)
 
 
+def result(request):
+    results = Result.objects.all()
+    return render(request, 'result.html', {'result': results})
